29f7b6fdb394363bc14484f12b2ab19ee49f92d8
= <anton@lyxit.se>
2015-02-08 23:32:39 +0100

    Actually loading (placeholder) questions with js

--- js/quiz.js
+++ js/quiz.js
@@ -1,13 +1,62 @@
 $(function() {
   /*
+  * TEMP - TODO: REMOVE ME
+  <div class="col-xs-12">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h2 class="panel-title question" id="1">Question?</h2>
+      </div>
+      <div class="panel-body">
+        <ul class="nav nav-pills nav-justified answers">
+          <li role="presentation" class="answer"><a href="#">Dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </a></li>
+          <li role="presentation" class="answer"><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</a></li>
+          <li role="presentation" class="answer"><a href="#">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</a></li>
+          <li role="presentation" class="answer"><a href="#">Ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  */
+
+  /*
+  * SETUP
+  */
+  var questions;
+
+  fetchQuestions();
+  showQuestions();
+
+  /*
   * BACKEND
   */
 
   /* TODO: Fetch questions from data source */
+  function fetchQuestions() {
+    questions = [createQuestionFromArrayData(''), createQuestionFromArrayData(''), createQuestionFromArrayData(''),];
+  }
 
+  function createQuestionFromArrayData(data) {
+    var question = new Object();
+
+    question.number = 1;
+    question.question = "Question?"; //Question from data
+    var answers = ['1', '2', '3', '4']; //Replace with answers from data
+    question.answers = answers;
+    question.correctAnswer = 1;
+
+    return question;
+  }
   /* TODO: Insert questions to DOM */
+  function showQuestions() {
+    questions.forEach(function(q) {
+      showQuestion(q);
+    });
+  }
+  function showQuestion(question) {
+    var htmlQuestion = '<div class="col-xs-12"><div class="panel panel-default"><div class="panel-heading"><h2 class="panel-title question" id="' + question.number + '">' + question.question + '</h2></div><div class="panel-body"><ul class="nav nav-pills nav-justified answers"><li role="presentation" class="answer"><a href="#">' + question.answers[0] + '</a></li><li role="presentation" class="answer"><a href="#">' + question.answers[1] + '</a></li><li role="presentation" class="answer"><a href="#">' + question.answers[2] + '</a></li><li role="presentation" class="answer"><a href="#">' + question.answers[3] + '</a></li></ul></div></div></div>';
 
-  function
+    $('#questions').append(htmlQuestion);
+  }
 
   /*
   * UI
