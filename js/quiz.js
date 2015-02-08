835f89d689f89eecbb46bb840430e9a5c626b84a
= <anton@lyxit.se>
2015-02-08 23:54:26 +0100

    Small fix

--- js/quiz.js
+++ js/quiz.js
@@ -22,41 +22,10 @@ $(function() {
   * SETUP
   */
   var questions;
+  var answers;
 
   fetchQuestions();
   showQuestions();
-
-  /*
-  * BACKEND
-  */
-
-  /* TODO: Fetch questions from data source */
-  function fetchQuestions() {
-    questions = [createQuestionFromArrayData(''), createQuestionFromArrayData(''), createQuestionFromArrayData(''),];
-  }
-
-  function createQuestionFromArrayData(data) {
-    var question = new Object();
-
-    question.number = 1;
-    question.question = "Question?"; //Question from data
-    var answers = ['1', '2', '3', '4']; //Replace with answers from data
-    question.answers = answers;
-    question.correctAnswer = 1;
-
-    return question;
-  }
-  /* TODO: Insert questions to DOM */
-  function showQuestions() {
-    questions.forEach(function(q) {
-      showQuestion(q);
-    });
-  }
-  function showQuestion(question) {
-    var htmlQuestion = '<div class="col-xs-12"><div class="panel panel-default"><div class="panel-heading"><h2 class="panel-title question" id="' + question.number + '">' + question.question + '</h2></div><div class="panel-body"><ul class="nav nav-pills nav-justified answers"><li role="presentation" class="answer"><a href="#">' + question.answers[0] + '</a></li><li role="presentation" class="answer"><a href="#">' + question.answers[1] + '</a></li><li role="presentation" class="answer"><a href="#">' + question.answers[2] + '</a></li><li role="presentation" class="answer"><a href="#">' + question.answers[3] + '</a></li></ul></div></div></div>';
-
-    $('#questions').append(htmlQuestion);
-  }
 
   /*
   * UI
@@ -72,4 +41,41 @@ $(function() {
 
     /* TODO: Save selected answer to answers */
   });
+
+  /*
+  * BACKEND
+  */
+
+  /* TODO: Fetch questions from data source */
+  function fetchQuestions() {
+    questions = [createQuestionFromArrayData(''), createQuestionFromArrayData(''), createQuestionFromArrayData(''),];
+  }
+
+  function createQuestionFromArrayData(data) {
+    var question = new Object();
+
+    question.number = 1;
+    question.question = "Question?"; //TODO: Question from data
+    var answers = ['1', '2', '3', '4']; //TODO: Replace with answers from data
+    question.answers = answers;
+    question.correctAnswer = 1;
+
+    return question;
+  }
+
+  function showQuestions() {
+    questions.forEach(function(q) {
+      showQuestion(q);
+    });
+  }
+
+  function showQuestion(question) {
+    var htmlQuestion = '<!-- Question START --><div class="row" id="' + question.number + '"><div class="col-xs-12"><div class="panel panel-default"><div class="panel-heading"><h2 class="panel-title question">' + question.question + '</h2></div><div class="panel-body"><ul class="nav nav-pills nav-justified answers"><li role="presentation" class="answer"><a href="#">' + question.answers[0] + '</a></li><li role="presentation" class="answer"><a href="#">' + question.answers[1] + '</a></li><li role="presentation" class="answer"><a href="#">' + question.answers[2] + '</a></li><li role="presentation" class="answer"><a href="#">' + question.answers[3] + '</a></li></ul></div></div></div></div><!-- Question END -->';
+
+    $('#questions').append(htmlQuestion);
+  }
+
+  function didSelectAnswer(answer, questionID) {
+
+  }
 });
