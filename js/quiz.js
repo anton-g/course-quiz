de634cd25b04de936db9eae5a4f1a0ab37838730
= <anton@lyxit.se>
2015-02-09 19:57:27 +0100

    Shows results and correct answers

--- js/quiz.js
+++ js/quiz.js
@@ -1,24 +1,5 @@
 $(function() {
   /*
-  * TEMP - TODO: REMOVE ME
-  <div class="col-xs-12">
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h2 class="panel-title question" id="1">Question?</h2>
-      </div>
-      <div class="panel-body">
-        <ul class="nav nav-pills nav-justified answers">
-          <li role="presentation" class="answer"><a href="#">Dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </a></li>
-          <li role="presentation" class="answer"><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</a></li>
-          <li role="presentation" class="answer"><a href="#">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</a></li>
-          <li role="presentation" class="answer"><a href="#">Ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  */
-
-  /*
   * SETUP
   */
   var questions = new Array();
@@ -108,6 +89,7 @@ $(function() {
     question.userAnswer = answerID;
   }
 
+  //TODO: Är lite lång alltså, refaktorera
   function correctAnswers() {
     var questions = $('#questions').find('.question');
 
@@ -125,18 +107,25 @@ $(function() {
       if (question.correct) {
         numCorrectAnswers++;
 
-
         $(this).find('.panel').removeClass('panel-default').addClass('panel-success');
         //TODO: Important css? ;<
         $(selectedAnswerButton).addClass('green');
+
+        $(selectedAnswerButton).find('a').html('<span class="glyphicon glyphicon-ok pull-left" aria-hidden="true"></span>' + $(selectedAnswerButton).find('a').html());
       }
       else {
         $(this).find('.panel').removeClass('panel-default').addClass('panel-danger');
         //TODO: Important css? ;<
         $(selectedAnswerButton).addClass('red');
+
+        $(selectedAnswerButton).find('a').html('<span class="glyphicon glyphicon-remove pull-left" aria-hidden="true"></span>' + $(selectedAnswerButton).find('a').html());
+
+        //Show correct answer
+        var incorrectAnswer = $(this).find('li[data-answerid=' + question.correctAnswer + '] a');
+        incorrectAnswer.html(incorrectAnswer.html() + ' <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>');
       }
 
-      //Disable all answers
+      //Disable stuff
       $('.answer').addClass('disabled');
       $('#answerBtn').attr('disabled', 'disabled');
     });
@@ -146,11 +135,21 @@ $(function() {
 
   function showResults() {
     var numCorrect = numCorrectAnswers + '/' + questions.length;
-    var percent
+    var percent = ((numCorrectAnswers / questions.length) * 100);
 
-    var htmlResult = '<div class="row result"><div class="col-xs-12"><div class="panel panel-primary"><div class="panel-heading"><h2 class="panel-title">Resultat</h2></div><div class="panel-body">' + numCorrect + '</div></div></div></div>';
+    var htmlStart = '<div class="row result"><div class="col-xs-12"><div class="panel panel-primary"><div class="panel-heading"><h2 class="panel-title">Resultat</h2></div><div class="panel-body">'
+    var htmlBody = '<div class="row"><div class="col-xs-9">Antal rätt: ' + numCorrect + ' (' + percent +'%)</div><div class="col-xs-3"><button class="btn btn-large btn-primary pull-right" id="resetBtn">Try again!</button></div></div>';
+    var htmlEnd = '</div></div></div></div>';
+
+    var htmlResult = htmlStart + htmlBody + htmlEnd;
 
     $('#questions').prepend(htmlResult);
+
+    $('#resetBtn').click(function(e) {
+      e.preventDefault();
+
+      location.reload();
+    });
   }
 
   function questionFromID(id) {
