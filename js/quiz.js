03d08be924286be79812ab216bcb12c935a4cffe
= <anton@lyxit.se>
2015-02-09 11:32:03 +0100

    Questions loaded from XML

--- js/quiz.js
+++ js/quiz.js
@@ -21,47 +21,26 @@ $(function() {
   /*
   * SETUP
   */
-  var questions;
-  var answers;
+  var questions = new Array();
+  var userAnswers;
 
-  fetchQuestions();
-  showQuestions();
+  fetchQuestions(function() {
+    showQuestions();
 
-  /*
-  * UI
-  */
-  $('.answer').click(function() {
-    var parent = $(this).parent();
+    $('.answer').click(function(e) {
+      e.preventDefault();
 
-    parent.children().each(function(a) {
-      $(this).removeClass('active');
+      var parent = $(this).parent();
+
+      parent.children().each(function(a) {
+        $(this).removeClass('active');
+      });
+
+      $(this).addClass('active');
+
+      /* TODO: Save selected answer to answers */
     });
-
-    $(this).addClass('active');
-
-    /* TODO: Save selected answer to answers */
   });
-
-  /*
-  * BACKEND
-  */
-
-  /* TODO: Fetch questions from data source */
-  function fetchQuestions() {
-    questions = [createQuestionFromArrayData(''), createQuestionFromArrayData(''), createQuestionFromArrayData(''),];
-  }
-
-  function createQuestionFromArrayData(data) {
-    var question = new Object();
-
-    question.number = 1;
-    question.question = "Question?"; //TODO: Question from data
-    var answers = ['1', '2', '3', '4']; //TODO: Replace with answers from data
-    question.answers = answers;
-    question.correctAnswer = 1;
-
-    return question;
-  }
 
   function showQuestions() {
     questions.forEach(function(q) {
@@ -73,6 +52,43 @@ $(function() {
     var htmlQuestion = '<!-- Question START --><div class="row" id="' + question.number + '"><div class="col-xs-12"><div class="panel panel-default"><div class="panel-heading"><h2 class="panel-title question">' + question.question + '</h2></div><div class="panel-body"><ul class="nav nav-pills nav-justified answers"><li role="presentation" class="answer"><a href="#">' + question.answers[0] + '</a></li><li role="presentation" class="answer"><a href="#">' + question.answers[1] + '</a></li><li role="presentation" class="answer"><a href="#">' + question.answers[2] + '</a></li><li role="presentation" class="answer"><a href="#">' + question.answers[3] + '</a></li></ul></div></div></div></div><!-- Question END -->';
 
     $('#questions').append(htmlQuestion);
+  }
+
+  /*
+  * BACKEND
+  */
+
+  /* TODO: Fetch questions from data source */
+  function fetchQuestions(complete) {
+    $.get('questions.xml', function(data) {
+      $(data).find('question').each(function() {
+        questions.push(createQuestionWith($(this)));
+      });
+
+      complete();
+    });
+  }
+
+  function createQuestionWith(data) {
+    var question = new Object();
+
+    question.number = data.attr('id');
+    question.question = data.attr('text');
+    var answerData = $(data).find('answer');
+
+    var answers = new Array();
+    $(answerData).each(function(index) {
+      if ($(this).attr('correct') == 'YES') {
+        question.correctAnswer = index + 1;
+      }
+
+      answers.push($(this).text());
+    });
+    question.answers = answers;
+
+    console.log (question);
+
+    return question;
   }
 
   function didSelectAnswer(answer, questionID) {
